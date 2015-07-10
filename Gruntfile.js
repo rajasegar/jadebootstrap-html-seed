4b6fead39b18c44b2aef47900e3594a813ed1b39
Rajasegar <rajasegar.c@gmail.com>
2015-07-10 12:30:19 +0530

    removed local jade bootstrap files, now it is taken from the central repository using bower which can be installed using grunt commands

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,21 @@ module.exports = function(grunt){
 	grunt.initConfig({
 		pkg:grunt.file.readJSON('package.json'),
 		
-		
+		bower:{
+			install:{
+				options:{
+					targetDir:'./jade'
+				}
+			}
+		},
+		copy:{
+			main:{
+				files:[
+					// copying jade-bootstrap css files to html folder
+					{ expand:true,cwd:'bower_components/jade-bootstrap/css/',src:['*.css'],dest:'html/css/',filter:'isFile'}
+				]
+			}
+		},
 		
 		jade:{
 			compile:{
@@ -40,6 +54,8 @@ module.exports = function(grunt){
 	});
 	
 	// Load the plugin that provides the "jade" task
+	grunt.loadNpmTasks("grunt-bower-task");
+	grunt.loadNpmTasks("grunt-contrib-copy");
 	grunt.loadNpmTasks("grunt-contrib-watch");
 	grunt.loadNpmTasks("grunt-contrib-jade");
 	
@@ -47,6 +63,7 @@ module.exports = function(grunt){
 	
 	// Default tasks
 	grunt.registerTask('default',['jade','watch']);
+	grunt.registerTask('install',['bower','copy']);
 	
 	
 	
